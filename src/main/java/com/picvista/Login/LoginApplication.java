3aabb198a951c514da6466ad706388d9f5e55e1f
turkuazsengul <turkuazsengul@gmail.com>
2024-01-14 02:42:59 +0300

    config cloud server integration completed

--- src/main/java/com/picvista/Login/LoginApplication.java
+++ src/main/java/com/picvista/Login/LoginApplication.java
@@ -3,6 +3,7 @@ package com.picvista.Login;
 import com.picvista.Login.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
